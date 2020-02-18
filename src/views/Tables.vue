0f9557fb1b2155a2fa3083ff4ae00214f338cbd5
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2020-02-18 14:45:25 -0500

    Revert "made it so the dots are individule"
    This reverts commit d40a6203c8acef66ca206f0f292bd89b14eb7983.

--- src/views/Tables.vue
+++ src/views/Tables.vue
@@ -5,8 +5,7 @@
     <div class="column" v-for="table in tables" :key="table.id">
       <div class="table"  @click="hostTable(table.id, user.displayName)">
         <div>{{ table.id }}</div>
-        <span v-if="table.isOpen" class="green-dot" />
-        <span v-else class="red-dot" />
+        <span :class="toggleDot" />
         <div>{{ table.serverId }}</div>
       </div>
     </div>
