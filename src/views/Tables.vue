121161a79084db9252b86405760718c8a1ad88bf
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2020-02-18 14:44:42 -0500

    Revert "seeing if i can add space for looks"
    This reverts commit 57fce964b8ebecff8086d2ed2decc0caee45f17b.

--- src/views/Tables.vue
+++ src/views/Tables.vue
@@ -9,7 +9,7 @@
         <div>
           <span v-if="table.isOpen" class="green-dot" />
           <span v-else class="red-dot" />
-          <b>&nbsp;{{ table.id }}</b>
+          <b>{{ table.id }}</b>
         </div>
         <div>{{ table.serverId }}</div>
       </div>
