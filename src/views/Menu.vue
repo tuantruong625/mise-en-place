d145a434c297a35dabe1b7e366982569f08a24b9
Tuan Truong <tuan.truong625@gmail.com>
2020-03-08 23:20:31 -0400

    Add grid layout

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -9,35 +9,45 @@
       </nav>
     </header>
 
-    <div v-show="showFood" class="food-header">
-      <span v-for="menu in restaurant.menu" :key="menu.id">
-        <span class="food-header__link" :class="{ active: activeHeader(menu.title) }" @click="selectedHeader(menu.title)">{{ menu.title }}</span>
-      </span>
-    </div>
+    <main class="menu-items">
+      <div v-show="showFood" class="menu-items__header">
+        <span v-for="menu in restaurant.menu" :key="menu.id">
+          <span class="menu-items__header--link" :class="{ active: activeHeader(menu.title) }" @click="selectedHeader(menu.title)">{{ menu.title }}</span>
+        </span>
+      </div>
 
-    <div v-show="showFood">
-      <ul class="menu-item-container" v-for="menu in filteredList" :key="menu.id">
-        <li class="menu-item-card" v-for="item in menu.menuItems" :key="item.id">
-          <span class="menu-item-card__name">{{ item.name }}</span>
-          <span class="menu-item-card__price">${{ item.price }}</span>
+      <ul v-show="showFood" class="menu-item-wrapper" v-for="menu in filteredList" :key="menu.id">
+        <li class="menu-item-wrapper__card" v-for="item in menu.menuItems" :key="item.id" @click="highlighted(item)">
+          <span class="menu-item-wrapper__card--name">{{ item.name }}</span>
+          <span class="menu-item-wrapper__card--price">${{ item.price }}</span>
         </li>
       </ul>
-    </div>
 
-    <div v-show="showDrink" class="food-header">
-      <span v-for="menu in drinks.menu" :key="menu.id">
-        <span class="food-header__link" :class="{ active: activeHeader(menu.title) }" @click="selectedHeader(menu.title)">{{ menu.title }}</span>
-      </span>
-    </div> 
+      <div v-show="showDrink" class="menu-items__header">
+        <span v-for="menu in drinks.menu" :key="menu.id">
+          <span class="menu-items__header--link" :class="{ active: activeHeader(menu.title) }" @click="selectedHeader(menu.title)">{{ menu.title }}</span>
+        </span>
+      </div> 
 
-    <div v-show="showDrink">
-      <ul class="menu-item-container" v-for="menu in filteredDrinkList" :key="menu.id">
-        <li class="menu-item-card" v-for="item in menu.menuItems" :key="item.id">
-          <span class="menu-item-card__name">{{ item.name }}</span>
-          <span class="menu-item-card__price">${{ item.price }}</span>
+      <ul v-show="showDrink" class="menu-item-wrapper" v-for="menu in filteredDrinkList" :key="menu.id">
+        <li class="menu-item-wrapper__card" v-for="item in menu.menuItems" :key="item.id">
+          <span class="menu-item-wrapper__card--name">{{ item.name }}</span>
+          <span class="menu-item-wrapper__card--price">${{ item.price }}</span>
         </li>
       </ul>
-    </div>
+
+      <div class="button-wrapper">
+        <button class="action-button increment-button">+</button>
+        <span class="action-button quantity">0</span>
+        <button class="action-button decrement-button">-</button>
+        <button class="action-button modification-button">Modification</button>
+        <button class="action-button add-button">Add</button>
+      </div>
+    </main>
+
+    <aside class="menu-order">
+      <h2>Order Number #12312</h2>
+    </aside>
   </section>
 </template>
 
@@ -55,6 +65,8 @@ export default {
       showDrink: false,
       drinks: {},
       search: 'Appetizers',
+      isHighlighted: false,
+      order: [],
     };
   },
   watch: {
@@ -85,6 +97,12 @@ export default {
     },
   },
   methods: {
+    highlighted(item) {
+    
+    },
+    addToOrder(item) {
+      this.order.push(item);
+    },
     selectedHeader(header) {
       this.search = header;
     },
@@ -163,73 +181,127 @@ export default {
     color: #495057;
     margin-top: 5.8rem;
     background-image: url("data:image/svg+xml,%3Csvg width='42' height='44' viewBox='0 0 42 44' xmlns='http://www.w3.org/2000/svg'%3E%3Cg id='Page-1' fill='none' fill-rule='evenodd'%3E%3Cg id='brick-wall' fill='%2376c9ba' fill-opacity='0.1'%3E%3Cpath d='M0 0h42v44H0V0zm1 1h40v20H1V1zM0 23h20v20H0V23zm22 0h20v20H22V23z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+    display: grid;
+    grid-template-columns: auto auto 400px;
+    grid-template-rows: 5rem auto;
+    grid-template-areas:
+    "header header order"
+    "items items order"; 
   }
 
   .menu-header {
+    grid-area: header;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
-    flex-direction: column;
-    margin-bottom: 0.25rem;
-    padding-top: 2rem;
+    padding: 1rem;
 
     &__title {
-      font-size: 1.5rem;
-      font-weight: 600;
+      font-size: 1.25rem;
+      color: #adb5bd;
     }
   }
 
   .menu-nav {
-    margin: 1rem 0;
-    &__link {
-      margin: 0 0.5rem;
+    &__link a {
+      color: #495057;
+      text-decoration: none;
+      margin: 0 0.25rem; 
     }
   }
 
-  .menu-item-container {
-    margin: 1rem;
+  .menu-items {
     display: grid;
-    grid-template-areas:
-    "header header header";
+    grid-template-rows: 2rem auto 5rem;
+    grid-area: items;
+    
 
-    &__title {
-      margin-right: auto;
-      font-size: 1.25rem;
-      grid-area: header;
+    &__header {
+      display: flex;
+      justify-content: space-around;
+
+      &--link {
+        font-size: 1.25rem;
+        cursor: pointer;
+        color: #ced4da;
+      }
     }
   }
 
-  .menu-item-card {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 1rem;
-    padding: 0 1rem;
-    background: #EFEEEE;
-    border: 1px solid rgba(255, 255, 255, 0.2);
-    box-shadow: 6px 6px 16px rgba(209, 205, 199, 0.5), -6px -6px 16px rgba(255, 255, 255, 0.5);
-    border-radius: 10px;
-    width: 250px;
-    height: 60px;
+  .menu-item-wrapper {
+    display: grid;
+    align-self: start;
+    grid-template-columns: auto auto;  
 
-    &__name {
-      text-transform: capitalize;
-    }
-  }
-
-  .food-header {
-    display: flex;
-    justify-content: space-around;
-
-    &__link {
-      font-size: 1.25rem;
-      color: #868e96;
-      cursor: pointer;
+    &__card {
+      justify-self: center;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin: 1rem;
+      padding: 0 1rem;
+      background: #EFEEEE;
+      border: 1px solid rgba(255, 255, 255, 0.2);
+      box-shadow: 6px 6px 16px rgba(209, 205, 199, 0.5), -6px -6px 16px rgba(255, 255, 255, 0.5);
+      border-radius: 10px;
+      width: 250px;
+      height: 60px;
     }
   }
 
   .active {
     text-decoration: underline #76c9ba;
     color: #495057;
+  }
+
+  .button-wrapper {
+    align-self: end;
+    justify-self: center;
+    margin-bottom: 1rem;
+    box-shadow: 6px 6px 16px rgba(209, 205, 199, 0.5), -6px -6px 16px rgba(255, 255, 255, 0.5);
+    border-radius: 20px;
+  }
+
+  .action-button {
+    height: 2.5rem;
+    background: #EFEEEE;
+    border: none;
+    color: #73c9ba;
+  }
+
+  .increment-button {
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    color: #73c9ba;
+  }
+
+  .quantity {
+    color: #868E96;
+  }
+
+  .decrement-button {
+
+  }
+
+  .modification-button {
+    color: #73c9ba;
+
+  }
+
+  .add-button {
+    background: #73c9ba;
+    color: #EFEEEE;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
+
+  .menu-order {
+    grid-area: order;
+    border: 1px solid green;
+    background: #EFEEEE;
+    border: 1px solid rgba(255, 255, 255, 0.2);
+    box-shadow: 6px 6px 16px rgba(209, 205, 199, 0.5), -6px -6px 16px rgba(255, 255, 255, 0.5);
+    border-radius: 10px;
+    margin: 1.5rem;
   }
 </style>
